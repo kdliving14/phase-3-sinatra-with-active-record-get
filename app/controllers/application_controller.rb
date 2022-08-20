420d5726bcbc643f0810faf1145613edde0026a9
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-08-20 15:54:51 -0700

    commit tree deb8ccc7c4e9c5cfe74b0f6a9c37ae9244c595e9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,26 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    # game.to_json
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
+
 end
